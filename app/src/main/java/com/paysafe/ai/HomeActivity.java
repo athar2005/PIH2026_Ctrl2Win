0f2fb236f88aa779732d10488b5d40d1fcd4a917
Athar Shaikh <athar21sd11@gmail.com>
2026-02-28 19:52:58 +0530

    Initial PaySafe AI App Setup
    ✔ Splash Screen
    ✔ Home Screen UI
    ✔ Result Screen
    ✔ Screenshot Upload Flow
    ✔ Navigation Working

---
+++ app/src/main/java/com/paysafe/ai/HomeActivity.java
@@ -0,0 +1,13 @@
+package com.paysafe.ai;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class HomeActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
